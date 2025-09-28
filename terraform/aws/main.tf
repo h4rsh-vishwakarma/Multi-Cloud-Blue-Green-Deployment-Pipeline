1294c2ad8690819bd8c36ec1cdb8828dfd50d414
Harsh Vishwakarma <144579460+h4rsh-vishwakarma@users.noreply.github.com>
2025-09-28 17:32:57 +0530

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# यह एक सर्वर बनाएगा
+# Make a server
 resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
   instance_type = "t2.micro" # Free tier
 
-  # सर्वर स्टार्ट होते ही यह स्क्रिप्ट चलेगी
+  # Start the server and run this script
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y docker
               service docker start
-              # Docker Hub से हमारी इमेज डाउनलोड करके चलाओ
+              # Download the image form DockerHub
               docker run -d -p 80:80 --rm -e ENV_COLOR=${var.env_color} your_dockerhub_username/my-flask-app:${var.docker_image_tag}
               EOF
 
